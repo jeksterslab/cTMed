9b089b909e9219e3fe4d71556995282fdf4c55f0
jeksterslab <learn.jeksterslab@gmail.com>
2024-06-01 04:51:49 +0000

    Automated build.

--- .setup/cpp/cTMed-test-phi.cpp
+++ .setup/cpp/cTMed-test-phi.cpp
@@ -5,47 +5,7 @@
 
 #include <RcppArmadillo.h>
 // [[Rcpp::depends(RcppArmadillo)]]
-
-//' Test the Drift Matrix
-//'
-//' Both have to be true for the function to return `TRUE`.
-//'   - Test that the real part of all eigenvalues of \eqn{\boldsymbol{\Phi}}
-//'     are less than zero.
-//'   - Test that the diagonal values of \eqn{\boldsymbol{\Phi}}
-//'     are between 0 to negative inifinity.
-//'
-//' @author Ivan Jacob Agaloos Pesigan
-//'
-//' @param phi Numeric matrix.
-//'   The drift matrix (\eqn{\boldsymbol{\Phi}}).
-//'
-//' @examples
-//' phi <- matrix(
-//'   data = c(
-//'     -0.357, 0.771, -0.450,
-//'     0.0, -0.511, 0.729,
-//'     0, 0, -0.693
-//'   ),
-//'   nrow = 3
-//' )
-//' colnames(phi) <- rownames(phi) <- c("x", "m", "y")
-//' TestPhi(phi = phi)
-//' phi <- matrix(
-//'   data = c(
-//'     -6, 5.5, 0, 0,
-//'     1.25, -2.5, 5.9, -7.3,
-//'     0, 0, -6, 2.5,
-//'     5, 0, 0, -6
-//'   ),
-//'   nrow = 4
-//' )
-//' colnames(phi) <- rownames(phi) <- paste0("y", 1:4)
-//' TestPhi(phi = phi)
-//'
-//' @family Continuous Time Mediation Functions
-//' @keywords cTMed test
-//' @export
-// [[Rcpp::export]]
+// [[Rcpp::export(.TestPhi)]]
 bool TestPhi(const arma::mat& phi) {
   arma::vec phi_diag = phi.diag(0);
   arma::cx_vec eigenvalues_phi = arma::eig_gen(phi);
--- src/source.cpp
+++ src/source.cpp
@@ -375,47 +375,7 @@ Rcpp::NumericVector Med(const arma::mat& phi, const double& delta_t,
 
 #include <RcppArmadillo.h>
 // [[Rcpp::depends(RcppArmadillo)]]
-
-//' Test the Drift Matrix
-//'
-//' Both have to be true for the function to return `TRUE`.
-//'   - Test that the real part of all eigenvalues of \eqn{\boldsymbol{\Phi}}
-//'     are less than zero.
-//'   - Test that the diagonal values of \eqn{\boldsymbol{\Phi}}
-//'     are between 0 to negative inifinity.
-//'
-//' @author Ivan Jacob Agaloos Pesigan
-//'
-//' @param phi Numeric matrix.
-//'   The drift matrix (\eqn{\boldsymbol{\Phi}}).
-//'
-//' @examples
-//' phi <- matrix(
-//'   data = c(
-//'     -0.357, 0.771, -0.450,
-//'     0.0, -0.511, 0.729,
-//'     0, 0, -0.693
-//'   ),
-//'   nrow = 3
-//' )
-//' colnames(phi) <- rownames(phi) <- c("x", "m", "y")
-//' TestPhi(phi = phi)
-//' phi <- matrix(
-//'   data = c(
-//'     -6, 5.5, 0, 0,
-//'     1.25, -2.5, 5.9, -7.3,
-//'     0, 0, -6, 2.5,
-//'     5, 0, 0, -6
-//'   ),
-//'   nrow = 4
-//' )
-//' colnames(phi) <- rownames(phi) <- paste0("y", 1:4)
-//' TestPhi(phi = phi)
-//'
-//' @family Continuous Time Mediation Functions
-//' @keywords cTMed test
-//' @export
-// [[Rcpp::export]]
+// [[Rcpp::export(.TestPhi)]]
 bool TestPhi(const arma::mat& phi) {
   arma::vec phi_diag = phi.diag(0);
   arma::cx_vec eigenvalues_phi = arma::eig_gen(phi);
@@ -428,43 +388,7 @@ bool TestPhi(const arma::mat& phi) {
 
 #include <RcppArmadillo.h>
 // [[Rcpp::depends(RcppArmadillo)]]
-
-//' Test Stability
-//'
-//' The function computes the eigenvalues of the input matrix `x`.
-//' It checks if the real part of all eigenvalues is negative.
-//' If all eigenvalues have negative real parts,
-//' the system is considered stable.
-//'
-//' @author Ivan Jacob Agaloos Pesigan
-//'
-//' @param x Numeric matrix.
-//'
-//' @examples
-//' x <- matrix(
-//'   data = c(
-//'     -0.357, 0.771, -0.450,
-//'     0.0, -0.511, 0.729,
-//'     0, 0, -0.693
-//'   ),
-//'   nrow = 3
-//' )
-//' TestStable(x)
-//' x <- matrix(
-//'   data = c(
-//'     -6, 5.5, 0, 0,
-//'     1.25, -2.5, 5.9, -7.3,
-//'     0, 0, -6, 2.5,
-//'     5, 0, 0, -6
-//'   ),
-//'   nrow = 4
-//' )
-//' TestStable(x)
-//'
-//' @family Continuous Time Mediation Functions
-//' @keywords cTMed test
-//' @export
-// [[Rcpp::export]]
+// [[Rcpp::export(.TestStable)]]
 bool TestStable(const arma::mat& x) {
   arma::cx_vec eigenvalues = arma::eig_gen(x);
   return arma::all(arma::real(eigenvalues) < 0);
