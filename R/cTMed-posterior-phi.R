da0bf6d7b1f33c76c446e7c97b7c56addf0272ec
jeksterslab <jeksterslab@users.noreply.github.com>
2024-12-13 04:22:09 +0000

    Update src to output generated at d395535

--- R/cTMed-posterior-phi.R
+++ R/cTMed-posterior-phi.R
@@ -17,25 +17,34 @@
 #' @keywords cTMed posterior
 #' @export
 PosteriorPhi <- function(object) { # nocov start
-  stopifnot(
-    inherits(
-      x = object,
-      what = "ctStanFit"
+  if (requireNamespace("ctsem", quietly = TRUE)) {
+    stopifnot(
+      inherits(
+        x = object,
+        what = "ctStanFit"
+      )
     )
-  )
-  varnames <- object$ctstanmodelbase$latentNames
-  posterior <- ctsem::ctExtract(object)$pop_DRIFT
-  out <- lapply(
-    X = seq_len(dim(posterior)[1]),
-    FUN = function(i) {
-      phi <- posterior[i, , ]
-      colnames(phi) <- rownames(phi) <- varnames
-      return(phi)
-    }
-  )
-  class(out) <- c(
-    "ctmedposteriorphi",
-    class(out)
-  )
-  return(out)
+    varnames <- object$ctstanmodelbase$latentNames
+    posterior <- ctsem::ctExtract(object)$pop_DRIFT
+    out <- lapply(
+      X = seq_len(dim(posterior)[1]),
+      FUN = function(i) {
+        phi <- posterior[i, , ]
+        colnames(phi) <- rownames(phi) <- varnames
+        return(phi)
+      }
+    )
+    class(out) <- c(
+      "ctmedposteriorphi",
+      class(out)
+    )
+    return(out)
+  } else {
+    stop(
+      paste(
+        "The 'ctsem' package is required for this functionality.",
+        "Please install it manually."
+      )
+    )
+  }
 } # nocov end
