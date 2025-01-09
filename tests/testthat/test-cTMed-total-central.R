ee1eaabd246f80249a650514053132a32c62cc5b
jeksterslab <learn.jeksterslab@gmail.com>
2025-01-09 12:54:33 +0000

    Automated build.

---
+++ tests/testthat/test-cTMed-total-central.R
@@ -0,0 +1,86 @@
+## ---- test-total-central
+lapply(
+  X = 1,
+  FUN = function(i,
+                 text,
+                 tol) {
+    message(text)
+    delta_t <- 2
+    answer <- c(
+      0.7297791,
+      0.4398068,
+      0.0000000,
+      delta_t
+    )
+    phi <- matrix(
+      data = c(
+        -0.357, 0.771, -0.450,
+        0.0, -0.511, 0.729,
+        0, 0, -0.693
+      ),
+      nrow = 3
+    )
+    colnames(phi) <- rownames(phi) <- c("x", "m", "y")
+    testthat::test_that(
+      paste(text, "TotalCentral"),
+      {
+        testthat::skip_on_cran()
+        testthat::expect_true(
+          all(
+            (
+              answer - TotalCentral(
+                phi = phi,
+                delta_t = delta_t
+              )$output
+            ) <= tol
+          )
+        )
+      }
+    )
+    testthat::test_that(
+      paste(text, "TotalCentrals"),
+      {
+        testthat::skip_on_cran()
+        testthat::expect_true(
+          all(
+            (
+              answer - c(
+                as.vector(
+                  cTMed:::.TotalCentrals(
+                    phi = phi,
+                    delta_t = delta_t
+                  )
+                ),
+                delta_t
+              )
+            ) <= tol
+          )
+        )
+      }
+    )
+    total_central <- TotalCentral(
+      phi = phi,
+      delta_t = 1:5
+    )
+    print(total_central)
+    summary(total_central)
+    plot(total_central)
+    total_central <- TotalCentral(
+      phi = phi,
+      delta_t = 1
+    )
+    print(total_central)
+    summary(total_central)
+    testthat::test_that(
+      paste(text, "plot error"),
+      {
+        testthat::skip_on_cran()
+        testthat::expect_error(
+          plot(total_central)
+        )
+      }
+    )
+  },
+  text = "test-total-central",
+  tol = 0.01
+)
---
+++ tests/testthat/test-cTMed-trajectory.R
@@ -0,0 +1,40 @@
+## ---- test-trajectory
+lapply(
+  X = 1,
+  FUN = function(i,
+                 text) {
+    message(text)
+    testthat::test_that(
+      text,
+      {
+        testthat::skip_on_cran()
+        phi <- matrix(
+          data = c(
+            -0.357, 0.771, -0.450,
+            0.0, -0.511, 0.729,
+            0, 0, -0.693
+          ),
+          nrow = 3
+        )
+        colnames(phi) <- rownames(phi) <- c("x", "m", "y")
+        traj <- Trajectory(
+          mu0 = c(3, 3, -3),
+          time = 150,
+          phi = phi,
+          med = "m"
+        )
+        plot(traj)
+        traj <- Trajectory(
+          mu0 = c(3, 3, -3),
+          time = 25,
+          phi = phi,
+          med = "m"
+        )
+        print(traj)
+        summary(traj)
+        plot(traj, total = FALSE)
+      }
+    )
+  },
+  text = "test-trajectory"
+)
